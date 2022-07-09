44804c30a8c54d456f42dee0c7bfd5a5fbe6615e
Kangdyu <kangdyu99@gmail.com>
2022-07-09 18:36:40 +0900

    feat: base branch input을 추가하고 update 여부를 base branch와 비교하도록 함"

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,11 @@ echo "check: $INPUT_CHECK"
 if [ $INPUT_CHECK == "all" ]; then
     python grammar_checker/main.py $(find . -name '*.md')
 elif [ $INPUT_CHECK == "updated" ]; then
-    python grammar_checker/main.py $(git diff --name-only HEAD^ | grep '**/*.md')
+    echo "base branch: $INPUT_BASE_BRANCH"
+
+    git fetch origin "${INPUT_BASE_BRANCH}" --depth=1
+
+    python grammar_checker/main.py $(git diff --name-only ${INPUT_BASE_BRANCH} | grep '**/*.md')
 else
     echo "Invalid check value: $INPUT_CHECK"
     exit 1
