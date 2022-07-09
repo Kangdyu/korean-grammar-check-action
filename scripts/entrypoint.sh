d9fda8798c77a5892d2430e2c056a65e1023f363
Kangdyu <kangdyu99@gmail.com>
2022-07-09 16:07:17 +0900

    fix: entrypoint.sh if-else grammar

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,12 +2,10 @@
 
 echo "check: $INPUT_CHECK"
 
-if [ $INPUT_CHECK == "all" ]
-then
+if [ $INPUT_CHECK == "all" ]; then
     python grammar_checker/main.py $(find . -name '*.md')
-elif [ $INPUT_CHECK == "updated" ]
-then
-    # find updated files
+elif [ $INPUT_CHECK == "updated" ]; then
+    echo "check only updated value"
 else
     echo "Invalid check value: $INPUT_CHECK"
     exit 1
