0f88b964ca1fedda831c60c2090854560e0a8f6c
Kangdyu <kangdyu99@gmail.com>
2022-07-09 17:50:13 +0900

    feat: update된 파일만 검사하는 로직 추가

--- grammar_checker/main.py
+++ grammar_checker/main.py
@@ -4,6 +4,10 @@ from grammar import check_grammar
 
 files = sys.argv[1:]
 
+if len(files) == 0:
+    print("맞춤법 검사를 진행할 파일이 없습니다.")
+    exit(0)
+    
 print("다음 파일들에 대해 맞춤법 검사를 진행합니다.")
 print(files)
 print("=" * 50)
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,7 +5,7 @@ echo "check: $INPUT_CHECK"
 if [ $INPUT_CHECK == "all" ]; then
     python grammar_checker/main.py $(find . -name '*.md')
 elif [ $INPUT_CHECK == "updated" ]; then
-    echo "check only updated value"
+    python grammar_checker/main.py $(git diff --name-only HEAD | grep '**/*.md')
 else
     echo "Invalid check value: $INPUT_CHECK"
     exit 1
